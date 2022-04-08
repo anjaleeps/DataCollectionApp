dd54af7f5d98f5bce621911fd1320a026a7ab527
GayanJayakody <gayanjayakody13@gmail.com>
2022-04-09 00:01:17 +0530

    Added separate buttons to share CSV or data

--- app/src/main/java/com/example/datacollectionapp/screens/projectrecords/ProjectRecordsActivity.java
+++ app/src/main/java/com/example/datacollectionapp/screens/projectrecords/ProjectRecordsActivity.java
@@ -10,10 +10,14 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.example.datacollectionapp.R;
 import com.example.datacollectionapp.database.connectionmanagers.RecordFirestoreManager;
 import com.example.datacollectionapp.screens.projectlist.ProjectListActivity;
+import com.example.datacollectionapp.screens.record.ViewRecordActivity;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -31,15 +35,28 @@ import java.util.List;
 public class ProjectRecordsActivity extends AppCompatActivity {
 
     private RecordFirestoreManager recordFireStoreManager;
-    String projectId, projectName;
+    String projectId, projectName, recordId;
     private String TAG = "Record List";
     List<String[]> data;
+    private ListView recordListView;
+    private ArrayAdapter recordNamesAdapter;
+    ArrayList recordNames;
+    ArrayList recordIDs;
+    ArrayList imageLinks;
+    ArrayList audioLinks;
+    String selectedRecord;
+    public static final String Project_Id = "com.example.datacollectionapp.Project_Id";
+    public static final String Record_Id = "com.example.datacollectionapp.Record_Id ";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_records);
         data = new ArrayList<String[]>();
+        recordNames = new ArrayList();
+        recordIDs = new ArrayList();
+        imageLinks = new ArrayList();
+        audioLinks = new ArrayList();
         Intent intent = getIntent();
         projectId = intent.getStringExtra(ProjectListActivity.Project_Id);
         projectName = intent.getStringExtra(ProjectListActivity.Project_Name);
@@ -63,6 +80,11 @@ public class ProjectRecordsActivity extends AppCompatActivity {
         } catch (IllegalArgumentException e){
             Log.e("File Selector","The selected file can't be shared: " + e);
         }
+    }
+
+    private void downloadImage(){
+        String a = "https://firebasestorage.googleapis.com/v0/b/data-collection-app-6221e.appspot.com/o/images%2F079247ac-d16b-47dc-85b6-dabb860cfb29?alt=media&token=8187f920-1971-4032-92b5-b17df313146f";
+
     }
 
     private String createAndWriteFile(){
@@ -94,25 +116,57 @@ public class ProjectRecordsActivity extends AppCompatActivity {
                     String[] csvHeaders = headers.split(",");
                     data.add(csvHeaders);
                 }
+                int recordCount = 0;
                 for (QueryDocumentSnapshot document : task.getResult()) {
                     String dataRow = "";
                     //Log.d(TAG, document.getId() + " => " + document.getData());
-                    //Log.d(TAG, document.getId() + " => " + document.get("recordFields"));
+                    recordIDs.add(document.getId());
                     ArrayList abc = (ArrayList) document.get("recordFields");
-                    //Log.d(TAG, String.valueOf(abc.get(0).getClass().getName()));
-                    // Log.d(TAG, String.valueOf(abc.get(0)));
                     Log.d(TAG, String.valueOf(abc));
                     for(Object i : abc){
                         HashMap recordList = (HashMap) i;
                         Log.d(TAG, String.valueOf(recordList.get("fieldName")));
+                        if(((String) recordList.get("dataType")).contains("IMAGE")){
+                            imageLinks.add(recordList.get("value"));
+                        }
+                        else if(((String) recordList.get("dataType")).contains("AUDIO")){
+                            audioLinks.add(recordList.get("value"));
+                        }
+
                         dataRow += "," + (String) recordList.get("value");
                     }
+                    recordCount += 1;
+                    recordNames.add("Record " + String.valueOf(recordCount));
                     String[] csvData = dataRow.split(",");
                     data.add(csvData);
+                    recordView();
                 }
+
             } else {
                 Log.d(TAG, "Error getting documents: ", task.getException());
             }
         }
     };
+
+    private void recordView(){
+        recordListView = (ListView) findViewById(R.id.recordListView);
+        recordNamesAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, recordNames);
+        recordListView.setAdapter(recordNamesAdapter);
+        recordListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                selectedRecord = (String) recordNames.get(i);
+                //recordId = (String) recordIDs.get(i);
+                Log.i(TAG,selectedRecord);
+                goToViewRecord();
+            }
+        });
+    }
+
+    private void goToViewRecord(){
+        Intent intent = new Intent(this, ViewRecordActivity.class);
+        intent.putExtra(Project_Id, projectId);
+        intent.putExtra(Record_Id, recordId);
+        startActivity(intent);
+    }
 }
