acd8a0aa865c77b0e2a9697ec2e8efb7d769921e
GayanJayakody <gayanjayakody13@gmail.com>
2022-04-09 23:24:49 +0530

    Added ZipManager

---
+++ app/src/main/java/com/example/datacollectionapp/utils/ZipManager.java
@@ -0,0 +1,39 @@
+package com.example.datacollectionapp.utils;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class ZipManager {
+    private static int BUFFER_SIZE = 6 * 1024;
+
+    public static void zip(ArrayList<String> files, String zipFile) throws IOException {
+        BufferedInputStream origin = null;
+        ZipOutputStream out = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(zipFile)));
+        try {
+            byte data[] = new byte[BUFFER_SIZE];
+
+            for (String file : files) {
+                FileInputStream fi = new FileInputStream(file);
+                origin = new BufferedInputStream(fi, BUFFER_SIZE);
+                try {
+                    ZipEntry entry = new ZipEntry(file.substring(file.lastIndexOf("/") + 1));
+                    out.putNextEntry(entry);
+                    int count;
+                    while ((count = origin.read(data, 0, BUFFER_SIZE)) != -1) {
+                        out.write(data, 0, count);
+                    }
+                } finally {
+                    origin.close();
+                }
+            }
+        } finally {
+            out.close();
+        }
+    }
+}
